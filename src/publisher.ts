7060a990cf1a83c1d037f038effec23b5559364c
Kaxa Gvimradze <kaxagvimraze@gmail.com>
2023-05-23 13:07:03 -0700

    as server subscriber

--- src/publisher.ts
+++ src/publisher.ts
@@ -2,16 +2,16 @@ import { connect } from "mqtt"; // import connect from mqtt
 import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config();
 const client = connect("mqtt://" + process.env.MQTT_SERVER_URL); // create a client
+const _topic = process.env.TOPICTOREAD;
+const data = `{"operationType":"check","content":{"version":"00000001"}}`;
+const data1 = `{"operationType":"update","content":{"status":"ok"}}`;
 
+if (!_topic) {
+  throw new Error("No env file");
+}
 client.on("connect", function () {
-  client.subscribe("topic", function (err) {
-    // if (!err) {
-    setInterval(() => {
-      client.publish(
-        "liftos08765546789",
-        `{"a":"a", "a":"a", "a": "a", "a": "a"}`
-      );
-    }, 100);
-    // }
-  });
+  setInterval(() => {
+    console.log(data);
+    client.publish(_topic, data);
+  }, 3000);
 });
